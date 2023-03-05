bf29cfac123b9d93bd532b335b5866cd646141c4
morrazzzz <123486080+morrazzzz@users.noreply.github.com>
2023-03-05 18:07:36 +0300

    Update Device_Initialize.cpp

--- trunk/xr_3da/Device_Initialize.cpp
+++ trunk/xr_3da/Device_Initialize.cpp
@@ -32,7 +32,7 @@ void CRenderDevice::Initialize()
         AdjustWindowRect( &rc, m_dwWindowStyle, FALSE );
 
         // Create the render window
-		m_hWnd = CreateWindow( wndclass, "S.T.A.L.K.E.R.: Shadow Of Chernobyl", m_dwWindowStyle,
+		m_hWnd = CreateWindow( wndclass, "S.T.A.L.K.E.R.: ESM Engine", m_dwWindowStyle,
                                /*rc.left, rc.top, */CW_USEDEFAULT, CW_USEDEFAULT,
                                (rc.right-rc.left), (rc.bottom-rc.top), 0L,
                                0, hInstance, 0L );
