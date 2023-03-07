19321b896e3aa3c2660f5df3caaf9bfa22b5b230
morrazzzz <123486080+morrazzzz@users.noreply.github.com>
2023-03-07 19:01:01 +0300

    Почистил немного репозиторий от мусора

--- trunk/Include/xrAPI/xrAPI.h
+++ trunk/Include/xrAPI/xrAPI.h
@@ -1,5 +1,3 @@
-#ifndef	xrApi_included
-#define xrApi_included
 #pragma once
 
 #ifdef XRAPI_EXPORTS
@@ -8,9 +6,6 @@
 #define XRAPI_API __declspec(dllimport)
 #endif
 
-
-
-#ifndef _EDITOR
 //morrazzzz: Leave it like this for now!
 //class IRender_interface;
 //extern XRAPI_API IRender_interface*	Render; //Finish it!
@@ -19,7 +14,7 @@ class IRenderFactory;
 extern XRAPI_API IRenderFactory*	RenderFactory;
 
 class CDUInterface;
-extern XRAPI_API CDUInterface*	DU;
+extern XRAPI_API CDUInterface*	DU; //Finish it!
 
 //struct xr_token;
 //extern XRAPI_API xr_token*	vid_mode_token; //Finish it!
@@ -27,35 +22,11 @@ extern XRAPI_API CDUInterface*	DU;
 class IUIRender;
 extern XRAPI_API IUIRender*	UIRender;
 
-
-#ifndef	_EDITOR
 class CGameMtlLibrary;
 extern XRAPI_API CGameMtlLibrary *			PGMLib;
-#endif
 
 #ifdef DEBUG
 	class IDebugRender;
 	extern XRAPI_API IDebugRender*	DRender;
 #endif // DEBUG
 
-#else
-	class	CRender;
-    extern ENGINE_API CRender*	Render;
-
-   class IRenderFactory;
-    extern ENGINE_API IRenderFactory*	RenderFactory;
-#endif
-/*
-// This class is exported from the xrAPI.dll
-class XRAPI_API CxrAPI {
-public:
-	CxrAPI(void);
-	// TODO: add your methods here.
-};
-
-extern XRAPI_API int nxrAPI;
-
-XRAPI_API int fnxrAPI(void);
-*/
-
-#endif	//	xrApi_included
