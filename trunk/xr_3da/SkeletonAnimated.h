78665cf20cd3e89f8a811b7b173fc138320d9d78
morrazzzz <123486080+morrazzzz@users.noreply.github.com>
2023-03-13 01:02:15 +0300

    [CoP]: MotionID вынесен в animation_motion

--- trunk/xr_3da/SkeletonAnimated.h
+++ trunk/xr_3da/SkeletonAnimated.h
@@ -17,38 +17,7 @@ class   ENGINE_API CBoneInstanceAnimated;
 struct	ENGINE_API CKey;
 class	ENGINE_API CInifile;
 
-struct MotionID {
-private:
-	typedef const MotionID *(MotionID::*unspecified_bool_type) () const;
-public:
-	union{
-    	struct{
-    	    u16		idx:14;
-	        u16		slot:2;
-        };
-        u16			val;
-    };
-public:
-    				MotionID	(){invalidate();}
-    				MotionID	(u16 motion_slot, u16 motion_idx){set(motion_slot,motion_idx);}
-	ICF bool		operator==	(const MotionID& tgt) const {return tgt.val==val;}
-	ICF bool		operator!=	(const MotionID& tgt) const {return tgt.val!=val;}
-	ICF bool		operator<	(const MotionID& tgt) const {return val<tgt.val;}
-	ICF	bool		operator!	() const					{return !valid();}
-    ICF void		set			(u16 motion_slot, u16 motion_idx){slot=motion_slot; idx=motion_idx;}
-	ICF void		invalidate	() {val=u16(-1);}
-    ICF bool		valid		() const {return val!=u16(-1);}
-	const MotionID*	get			() const {return this;};
-	ICF	operator	unspecified_bool_type () const 
-	{
-		if (valid())	return &MotionID::get;
-		else			return 0;
-//		return(!valid()?0:&MotionID::get);
-	}
-};
-
-//. typedef u16 	BoneID;
-//. typedef u16 	PartitionID;
+#include "../Include/xrRender/animation_motion.h"
 
 //*** Run-time Blend definition *******************************************************************
 class ENGINE_API CBlend {
