58f270adec79750bfcd8846c583a17fdd17df4cd
morrazzzz <123486080+morrazzzz@users.noreply.github.com>
2024-04-25 22:05:34 +0300

    HWCaps из CoP(без NVIDIA и AMD кода)

--- trunk/xrRender/xrRender_R2/r2_rendertarget.cpp
+++ trunk/xrRender/xrRender_R2/r2_rendertarget.cpp
@@ -316,9 +316,9 @@ CRenderTarget::CRenderTarget		()
 		t_LUM_dest.create			(r2_RT_luminance_cur);
 
 		// create pool
-		for (u32 it=0; it<4; it++)	{
+		for (u32 it=0; it<HW.Caps.iGPUNum*2; it++)	{
 			string256					name;
-			sprintf						(name,"%s_%d",	r2_RT_luminance_pool,it	);
+			xr_sprintf						(name,"%s_%d",	r2_RT_luminance_pool,it	);
 			rt_LUM_pool[it].create		(name,	1,	1,	D3DFMT_R32F				);
 			u_setrt						(rt_LUM_pool[it],	0,	0,	0			);
 			CHK_DX						(HW.pDevice->Clear( 0L, NULL, D3DCLEAR_TARGET,	0x7f7f7f7f,	1.0f, 0L));
