eb03f21dc2a4e323da1e0a70cba723e7e4269400
morrazzzz <123486080+morrazzzz@users.noreply.github.com>
2024-04-27 02:18:25 +0300

    Fix accumulator

--- trunk/xrRender/xrRender_R2/r2_rendertarget_phase_accumulator.cpp
+++ trunk/xrRender/xrRender_R2/r2_rendertarget_phase_accumulator.cpp
@@ -17,15 +17,18 @@ void	CRenderTarget::phase_accumulator()
 		u32		clr4clear					= color_rgba(0,0,0,0);	// 0x00
 		CHK_DX	(HW.pDevice->Clear			( 0L, NULL, D3DCLEAR_TARGET, clr4clear, 1.0f, 0L));
 
+		//	Do it after the sun to preserve data.
+		/*
 		// Render emissive geometry, stencil - write 0x0 at pixel pos
-		RCache.set_xform_project					(Device.mProject); 
+		RCache.set_xform_project					(Device.mProject);
 		RCache.set_xform_view						(Device.mView);
-		// Stencil - write 0x1 at pixel pos - 
+		// Stencil - write 0x1 at pixel pos -
 		RCache.set_Stencil							( TRUE,D3DCMP_ALWAYS,0x01,0xff,0xff,D3DSTENCILOP_KEEP,D3DSTENCILOP_REPLACE,D3DSTENCILOP_KEEP);
 		//RCache.set_Stencil						(TRUE,D3DCMP_ALWAYS,0x00,0xff,0xff,D3DSTENCILOP_KEEP,D3DSTENCILOP_REPLACE,D3DSTENCILOP_KEEP);
 		RCache.set_CullMode							(CULL_CCW);
 		RCache.set_ColorWriteEnable					();
 		RImplementation.r_dsgraph_render_emissive	();
+		*/
 
 		// Stencil	- draw only where stencil >= 0x1
 		RCache.set_Stencil					(TRUE,D3DCMP_LESSEQUAL,0x01,0xff,0x00);
