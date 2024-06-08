444a0edd19c46c130d0f35b7f4aaf7e370646eee
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2024-06-08 17:24:27 +0300

    фикс релиза

--- trunk/xrGame/console_commands.cpp
+++ trunk/xrGame/console_commands.cpp
@@ -975,6 +975,7 @@ struct CCC_ClearSmartCastStats : public IConsole_Command {
 };
 #endif
 
+#ifdef DEBUG
 struct CCC_JumpToLevel : public IConsole_Command {
 	CCC_JumpToLevel(LPCSTR N) : IConsole_Command(N)  {};
 
@@ -1012,7 +1013,7 @@ struct CCC_JumpToLevel : public IConsole_Command {
 		}
 	}
 };
-
+#endif
 class CCC_Spawn : public IConsole_Command
 {
 public:
@@ -1581,9 +1582,9 @@ void CCC_RegisterCommands()
 	CMD4(CCC_FloatBlock,		"ph_rigid_break_weapon_factor",	&ph_console::phRigidBreakWeaponFactor	,			0.f		,1000000000.f	);
 	CMD4(CCC_Integer,			"ph_tri_clear_disable_count",	&ph_console::ph_tri_clear_disable_count	,			0,		255				);
 	CMD4(CCC_FloatBlock,		"ph_tri_query_ex_aabb_rate",	&ph_console::ph_tri_query_ex_aabb_rate	,			1.01f	,3.f			);
+	CMD1(CCC_JumpToLevel,	"jump_to_level"		)
 #endif // DEBUG
 
-	CMD1(CCC_JumpToLevel,	"jump_to_level"		)
 	CMD1(CCC_Spawn, "g_spawn")
 	CMD1(CCC_SpawnToInventory, "g_spawn_to_inventory")
 	CMD3(CCC_Mask,			"g_god",			&psActorFlags,	AF_GODMODE	)
