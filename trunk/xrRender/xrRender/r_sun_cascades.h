508cad4246a30aea2e6e55f090cac347a228d0eb
morrazzzz <123486080+morrazzzz@users.noreply.github.com>
2024-08-27 19:19:47 +0300

    * [NEW]: Новые sun cascade: Часть 1

--- trunk/xrRender/xrRender/r_sun_cascades.h
+++ trunk/xrRender/xrRender/r_sun_cascades.h
@@ -3,24 +3,30 @@
 namespace sun
 {
 
-struct ray
-{
-	ray( ) { }
-	ray( Fvector3 const& _P, Fvector3 const& _D ):	P(_P), D(_D) { }
+	struct ray
+	{
+		ray() { D.set(0, 0, 0); P.set(0, 0, 0); }
+		ray(Fvector3 const& _P, Fvector3 const& _D) : P(_P), D(_D) { }
 
-	Fvector3 D;
-	Fvector3 P;
-};
+		Fvector3 D;
+		Fvector3 P;
+	};
 
-struct cascade 
-{
-	cascade () : reset_chain( false )	{}
+	struct cascade
+	{
+		cascade()
+		{
+			xform.identity();
+			size = 0.0f;
+			bias = 0.0f;
+			reset_chain = false;
+		}
 
-	Fmatrix			xform;
-	xr_vector<ray>	rays;
-	float			size;
-	float			bias;
-	bool			reset_chain;
-};
+		Fmatrix	xform;
+		xr_vector<ray>	rays;
+		float			size;
+		float			bias;
+		bool			reset_chain;
+	};
 
 } //namespace sun
