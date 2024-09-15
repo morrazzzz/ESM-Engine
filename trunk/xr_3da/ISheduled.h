1a6d44a2af345291ddbdb77d484510d3c0c72110
morrazzzz <123486080+morrazzzz@users.noreply.github.com>
2024-09-15 10:56:50 +0300

    * [NEW]: Реализован отдельный Frame для шедулера

--- trunk/xr_3da/ISheduled.h
+++ trunk/xr_3da/ISheduled.h
@@ -7,7 +7,6 @@ public:
 		u32		t_min		:	14;		// minimal bound of update time (sample: 20ms)
 		u32		t_max		:	14;		// maximal bound of update time (sample: 200ms)
 		u32		b_RT		:	1;
-		u32		b_locked	:	1;
 	}	shedule;
 
 #ifdef DEBUG
