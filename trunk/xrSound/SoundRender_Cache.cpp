b8f7b64f7428dd642dee74faafa9e17ca4fc3de6
morrazzzz <123486080+morrazzzz@users.noreply.github.com>
2025-01-01 13:39:28 +0300

    Будем по другому заполнять cat в SoundRender Cache

--- trunk/xrSound/SoundRender_Cache.cpp
+++ trunk/xrSound/SoundRender_Cache.cpp
@@ -151,7 +151,8 @@ void	CSoundRender_Cache::cat_create	(cache_cat& cat, u32 bytes)
 	if	(bytes%_line)	cat.size += 1;
 	u32 allocsize		=	(cat.size&1)?cat.size+1:cat.size;
 	cat.table			=	xr_alloc<u16>(allocsize);
-	Memory.mem_fill32	(cat.table,0xffffffff,allocsize/2);
+//	Memory.mem_fill32	(cat.table,0xffffffff,allocsize/2);
+	std::fill_n(cat.table, allocsize / 2, 0xFFFFFFFF);
 }
 
 void	CSoundRender_Cache::cat_destroy	(cache_cat& cat)
