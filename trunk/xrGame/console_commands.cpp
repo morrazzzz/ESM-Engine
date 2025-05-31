3cebb0de0e8c656421742a56701ba97c19208e12
morrazzzz <123486080+morrazzzz@users.noreply.github.com>
2025-05-31 10:48:59 +0300

    g_crouch_toggle больше не актуальна

--- trunk/xrGame/console_commands.cpp
+++ trunk/xrGame/console_commands.cpp
@@ -1406,12 +1406,10 @@ void CCC_RegisterCommands()
 
 	CMD1(CCC_MemStats,			"stat_memory"			)
 	// game
-    psActorFlags.set(AF_CROUCH_TOGGLE, false);
 	psActorFlags.set(AF_WALK_TOGGLE, false);
 	psActorFlags.set(AF_SPRINT_TOGGLE, true);
 	psActorFlags.set(AF_ALWAYSRUN, true);
 
-	CMD3(CCC_Mask, "g_crouch_toggle", &psActorFlags, AF_CROUCH_TOGGLE)
 	CMD3(CCC_Mask, "g_walk_toggle", &psActorFlags, AF_WALK_TOGGLE)
 	CMD3(CCC_Mask, "g_sprint_toggle", &psActorFlags, AF_SPRINT_TOGGLE)
 	CMD3(CCC_Mask,				"g_always_run",			&psActorFlags,	AF_ALWAYSRUN)
