b1c9872b5ac205e2c66f8c06cbdb5ed9cc7e7653
morrazzzz <123486080+morrazzzz@users.noreply.github.com>
2025-06-17 12:14:18 +0300

    Удалён линейный поиск в constant таблицах рендера

--- trunk/xrRender/xrRender/r_constants.cpp
+++ trunk/xrRender/xrRender/r_constants.cpp
@@ -49,14 +49,7 @@ ref_constant R_constant_table::get	(LPCSTR S)
 }
 ref_constant R_constant_table::get	(shared_str& S)
 {
-	// linear search, but only ptr-compare
-	c_table::iterator I	= table.begin	();
-	c_table::iterator E	= table.end		();
-	for (; I!=E; ++I)	{
-		ref_constant	C		= *I;
-		if (C->name.equal(S))	return C;
-	}
-	return	0;
+	return get(S.c_str());
 }
 
 #if !defined(USE_DX10) && !defined(USE_DX11)
